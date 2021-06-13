e8eb56fed71757161fbc0277349f7deba62e75ec
Vazgen <Vazgen2004proweb@gmail.com>
2021-06-13 20:45:55 +0500

    Модальное окно

---
+++ scripts/order.js
@@ -0,0 +1,5 @@
+let order = document.querySelector('#modal__order');
+let body = document.querySelector('#body');
+let exit = document.querySelector('#order__exit');
+let btn = document.querySelector('');
+
